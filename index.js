5ca7fd5069317d47572468d9d1eea98561e3b47e
Róbert Selvek <robert.selvek@gmail.com>
2014-04-22 20:37:18 +0200

    Implemented .zip output, the zip crashes tellico, though

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-var martinus = require("martinus-scrape")
-	, zip = require("node-zip");
+var martinus = require("martinus-scrape"),
+	request = require("request"),
+	async = require("async");
 
-function booksToXML(books, cb) {
+function booksToXML(books) {
 	var xml = require("xmlbuilder").create("tellico",
 		{version: "1.0", encoding: "UTF-8"});
 	xml.att("xmlns", "http://periapsis.org/tellico/");
@@ -24,23 +25,54 @@ function booksToXML(books, cb) {
 		}
 	});
 
+	var imgs = coll.ele("images");
+
 	books.forEach(function (book) {
 		coll.ele(buildBookEntry(book));
+		imgs.ele({
+			image: {
+				"@format": "JPEG",
+				"@id": makeFilename(book.imageUrl)
+			}
+		})
 	})
 
-	console.log(xml.end({pretty: true, indent: "\t", newline: "\n"}));
+	return xml.end({pretty: true, indent: "\t", newline: "\n"});
 }
 
-function zipBooks(books, cb) {
-	booksToXML(books, function() {});
+function zipBooks(books, finalCb) {
+	xml = booksToXML(books);
+	zip = new require("node-zip")();
+	zip.file("tellico.xml", xml);
+	async.each(books, function (book, cb) {
+		if (!book.imageUrl) {
+			cb(null);
+		}
+		request({
+			url: book.imageUrl,
+			encoding: null
+		}, function (err, response, body) {
+			if (err) {
+				cb(err);
+			}
+			var filename = makeFilename(book.imageUrl);
+			zip.file("images/" + filename, body);
+			cb(null);
+		})}, function(err) {
+			var zipData = zip.generate({
+				type: "string"
+			});
+			if (err) {
+				process.stderr.write("Error when zipping images: " + err);
+			}
+			finalCb(err, zipData);
+		});
 }
 
 function buildBookEntry(book) {
-	var imageURL;
-	if (book.imageURL) {
-		var imageURL = book.imageURL.split("/");
-	} else {
-		imageURL = [""];
+	var imageUrl;
+	if (book.imageUrl) {
+		imageUrl = makeFilename(book.imageUrl)
 	}
 	var entry = {
 		entry: {
@@ -48,7 +80,7 @@ function buildBookEntry(book) {
 			publisher: book.publisher,
 			pub_year: book.pubYear,
 			isbn: book.ISBN,
-			cover: imageURL[imageURL.length - 1]
+			cover: imageUrl
 		}
 	};
 	if (book.authors.length > 0) {
@@ -64,6 +96,11 @@ function buildBookEntry(book) {
 	return entry;
 }
 
+function makeFilename(URL) {
+	var parts = URL.split("/");
+	return parts[parts.length - 1]
+}
+
 var searchTerm;
 
 if (process.argv[2] === "isbn") {
@@ -74,13 +111,12 @@ if (process.argv[2] === "isbn") {
 
 martinus.getBook(searchTerm, function(err, books) {
 	if (err) {
-		console.log("Error when downloading book info: " + err);
+		process.stderr.write("Error when downloading book info: " + err);
 		return;
 	}
 	zipBooks(books, function(err, zipFile) {
 		if (err) {
-			console.log("Error when zipping book info: " + err);
-			return;
+			//We just try to return everything possible
 		}
 		process.stdout.write(zipFile, "binary");
 	});
